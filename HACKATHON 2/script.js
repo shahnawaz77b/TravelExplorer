f4d5088b8ec7fef0567544af4bdf0b336578a3ad
Shahnawaz <149593912+shahnawaz77b@users.noreply.github.com>
2024-03-09 00:18:39 +0530

    Add files via upload

---
+++ HACKATHON 2/script.js
@@ -0,0 +1,11 @@
+function submitForm() {
+    
+    var destination = document.getElementById("destinationInput").value;
+    var date = document.getElementById("dateInput").value;
+
+    
+    console.log("Destination: " + destination);
+    console.log("Date: " + date);
+
+    
+}
